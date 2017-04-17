675409538cfd6562acb4568e9a91b8ac91d83100
Chaz Henricks <chazhenricks@gmail.com>
2017-04-17 12:48:15 -0500

    added functionality to console.log

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,24 +1,37 @@
-
+var character = document.getElementById("character");
+var height = document.getElementById("height");
+var treeButton = document.getElementById("tree-button");
 var tree = {};
 
-
-
-var treeButton = document.getElementById("tree-button");
-
 treeButton.addEventListener("click", function(){ createTree(tree);});
+
 
 function createTree(tree){
   tree.character = document.getElementById("character").value;
   tree.height = Number(document.getElementById("height").value);
+
+if (tree.character === " "){
+  character.className = "invalid";
+}
+if (tree.height === " "){
+  height.className = "invalid";
+}
+
   var treeOnPage = document.getElementById("content");
   var makeup = "";
+  var spaces = " ";
+  spaces = spaces.repeat(tree.height);
   var k = 1
+  var j = tree.height;
   for (var i=0; i<tree.height; i++){
-    makeup += "<p>"
-    makeup += tree.character.repeat(k);
-    makeup += "</p>";
-    k = k+2
+    makeup = tree.character.repeat(k);
+    k  = k +2;
+    console.log(spaces+makeup);
+    spaces = spaces.slice(0, j-1);
+    j--;
   }
-  console.log("makeup", makeup);
-  treeOnPage.innerHTML = makeup;
 };
+
+
+
+
