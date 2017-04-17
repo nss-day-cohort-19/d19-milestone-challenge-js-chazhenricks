066668a573c2ee1718268fb41d2ac0bbcba32635
Chaz Henricks <chazhenricks@gmail.com>
2017-04-17 11:49:32 -0500

    fixed JS to add * properly

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,10 +11,14 @@ function createTree(tree){
   tree.character = document.getElementById("character").value;
   tree.height = Number(document.getElementById("height").value);
   var treeOnPage = document.getElementById("content");
-  var makeup = "<p>";
+  var makeup = "";
+  var k = 1
   for (var i=0; i<tree.height; i++){
-   makeup += tree.character.repeat(i) + " ";
+    makeup += "<p>"
+    makeup += tree.character.repeat(k);
     makeup += "</p>";
+    k = k+2
   }
+  console.log("makeup", makeup);
   treeOnPage.innerHTML = makeup;
 };
